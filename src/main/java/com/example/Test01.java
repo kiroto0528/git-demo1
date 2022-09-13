b20f4164ebdda1d741ade05063ae7916cd2bcfb2
zhang-bo13009 <pipihao2b@qq.com>
2022-09-13 16:05:13 +0800

    Initial commit

---
+++ src/main/java/com/example/Test01.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("pp");
+        System.out.println("ee");
+        System.out.println("rrr");
+        System.out.println("rrr");
+    }
+}
